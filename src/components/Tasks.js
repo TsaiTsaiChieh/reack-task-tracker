db34284dca4ab04ac84d5b1dd1cc356e1e28ea33
Tsai-Chieh <d0177381@gmail.com>
2022-01-18 16:02:26 +0800

    :sparkles: global state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
-  ])
-
+const Tasks = ({tasks}) => {
   return (
     <>
       {tasks.map((task) => (
@@ -30,4 +10,14 @@ const Tasks = () => {
     </>
   )
 }
+
+Tasks.propTypes = {
+  tasks: {
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    day: PropTypes.string.isRequired,
+    reminder: PropTypes.bool.isRequired,
+  },
+}
+
 export default Tasks
