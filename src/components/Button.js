3340cc20e473c839a6bc8953a213ceddc79f1893
Tsai-Chieh <d0177381@gmail.com>
2022-01-18 13:55:47 +0800

    :sparkles: button events

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Button({color, text}) {
+function Button({color, text, onClick}) {
   return (
-    <button className='btn' style={{'backgroundColor': color}}>{text}</button>
+    <button className='btn'
+      onClick={onClick}
+      style={{'backgroundColor': color}}>
+      {text}
+    </button>
   )
 }
 
 Button.propTypes = {
   color: PropTypes.string,
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 }
 
 Button.defaultProps = {
