abe21f2554b2490c802d85c00b03d4abdcf894a7
Tsai-Chieh <d0177381@gmail.com>
2022-01-19 20:04:18 +0800

    :sparkles: form input state (controlled components)

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import PropTypes from 'prop-types'
 
 const AddTask = () => {
+  const [task, setTask] = useState('')
+  const [day, setDay] = useState('')
+  const [reminder, setReminder] = useState(false)
+
   return (
     <form className='add-form'>
       <div className='form-control'>
         <label>Task</label>
-        <input type="text" placeholder='Add Task'/>
+        <input 
+          type="text" 
+          value={task} 
+          placeholder='Add Task' 
+          onChange={(e) => setTask(e.target.value)}/>
       </div>
       <div className='form-control'>
         <label>Day & Time</label>
-        <input type="text" placeholder='Add Day & Time'/>
+        <input 
+          type="text" 
+          value={day} 
+          placeholder='Add Day & Time'
+          onChange={(e)=> setDay(e.target.value)}/>
       </div>
       <div className='form-control form-control-check'>
         <label>Reminder</label>
-        <input type="checkbox"/>
+        <input 
+          type="checkbox" 
+          value={reminder}
+          onChange={(e)=> setReminder(e.currentTarget.checked)}/>
       </div>
 
       <input className="btn btn-block" type="submit" value='Save Task'/>
